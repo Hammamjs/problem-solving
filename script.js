da0da1d9f5b1daf448ecf91e77dc569a749f7b79
unknown <hamamhussein10@gmail.com>
2022-09-23 12:49:49 +0300

    Remove space from String

---
+++ script.js
@@ -0,0 +1,15 @@
+
+// Diffiault :  Easy
+
+// remove all the spzce from the string
+
+function noSpace(x){
+return x.split(" ").join('');
+}
+
+
+
+
+console.log(noSpace(" ask q u es t io n ")) // run the Function
+
+    // result  : askquestion
